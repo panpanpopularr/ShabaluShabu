e34c03733b8ac3ab29b9dca7e3b2baf2a04e5ecf
TKanidpat <kanidpat.ten1@gmail.com>
2023-11-10 23:01:13 +0700

    sd

--- imgtopdf.py
+++ imgtopdf.py
@@ -1,10 +1,10 @@
 import os
 import img2pdf
 
-directory_path = "./img2pdf"
+directory_path = "./Images"
 
-image_files = [i for i in os.listdir(directory_path) if i.endswith(".jpg") if i.endswith(".jpeg") if i.endswith(".png")]
-
+image_files = [i for i in os.listdir(directory_path) if i.rfind(".jpg") if i.rfind(".jpeg") if i.rfind(".png")]
+print(image_files)
 pdf_data = img2pdf.convert(image_files)
 
 with open("output.pdf", "wb") as file:
